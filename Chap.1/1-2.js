a37f6b77ceb9b64fe6d0fe9b917a1f3ac3b86bb9
Gwanyeon Kim <kky9711@cau.ac.kr>
2022-04-22 18:55:03 +0900

    2

---
+++ Chap.1/1-2.js
@@ -0,0 +1,12 @@
+// 1-2
+// 변수 선언과 할당
+// 식별자 a
+
+var a;  // 변수 a 선언
+a = 'abc'; // 변수 a에 데이터 할당
+
+console.log(a)
+
+var a = 'abc';  // 한 문장으로 표현
+
+console.log(a)
---
+++ Chap.1/1-3.js
@@ -0,0 +1,12 @@
+// 1-3
+// 불변성
+// 식별자 a
+
+var a;  // 변수 a 선언
+a = 'abc'; // 변수 a에 데이터 할당
+
+console.log(a)
+
+var a = 'abc';  // 한 문장으로 표현
+
+console.log(a)
