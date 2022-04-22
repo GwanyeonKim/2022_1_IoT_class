54470702b3db16c72d8a64c587c6ba9dd8698848
Gwanyeon Kim <kky9711@cau.ac.kr>
2022-04-22 21:01:28 +0900

    변수 복사

---
+++ Chap.1/1-7.js
@@ -0,0 +1,24 @@
+// 1-7
+// 변수 복사
+
+var a =10;   // 선언
+var b = a;   // 복사
+
+console.log(a)
+console.log(b)
+
+//------------------------
+
+var obj1 = {  // 선언
+    c: 10,
+    d: 'ddd'
+};
+var obj2 = obj1;  // 복사
+
+console.log(obj1)
+console.log(obj1.c)
+console.log(obj1.d)
+
+console.log(obj2)
+console.log(obj2.c)
+console.log(obj2.d)
