90daabb5b6b0bc9ada331b4630ec2c25cbc4079c
Gwanyeon Kim <kky9711@cau.ac.kr>
2022-04-22 21:00:54 +0900

    중첩된 참조형 데이터의 프로퍼티 할당

---
+++ Chap.1/1-6.js
@@ -0,0 +1,14 @@
+// 1-6
+// 중첩된 참조형 데이터의 프로퍼티 할당
+
+var obj1 = {  // 참조형 데이터 obj1 선언
+    x: 3,
+    arr: [3, 4, 5]
+};
+
+console.log(obj1)
+console.log(obj1.x)
+console.log(obj1.arr)
+console.log(obj1.arr[0])
+console.log(obj1.arr[1])
+console.log(obj1.arr[2])
