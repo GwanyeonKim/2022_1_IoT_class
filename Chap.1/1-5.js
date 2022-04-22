9db108049ff116e40e3fa58d8d02294b58c4f2ef
Gwanyeon Kim <kky9711@cau.ac.kr>
2022-04-22 21:00:35 +0900

    참조형 데이터의 프로퍼티 재할당

---
+++ Chap.1/1-5.js
@@ -0,0 +1,16 @@
+// 1-5
+// 참조형 데이터의 프로퍼티 재할당
+
+var obj1 = {  // 참조형 데이터 obj1 선언
+    a: 1,
+    b: 'bbb'
+};
+
+console.log(obj1)
+console.log(obj1.a)
+console.log(obj1.b)
+
+
+obj1.a = 2; // 데이터 재할당
+
+console.log(obj1.a)
