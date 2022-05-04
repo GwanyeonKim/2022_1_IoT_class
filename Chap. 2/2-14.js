fcbc3f3e9c2b3e072a0b79f0d99894c0c020319c
Gwanyeon Kim <kky9711@cau.ac.kr>
2022-05-04 15:21:07 +0900

    스코프 체인 확인(1) - 크롬 전용

---
+++ Chap. 2/2-14.js
@@ -0,0 +1,15 @@
+// 2-14
+// 스코프 체인 확인(1) - 크롬 전용
+
+
+var a = 1;
+
+var outer = function () {
+    var b = 2;
+    var inner = function () {
+        console.dir(inner);
+    };
+    inner();
+};
+
+outer();
