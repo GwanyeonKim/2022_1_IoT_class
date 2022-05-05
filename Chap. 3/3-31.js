43d458e1b27d234b08b8bbee79c47ce4ec9d331b
Gwanyeon Kim <kky9711@cau.ac.kr>
2022-05-05 21:29:28 +0900

    콜백 함수와 함께 thisArg를 인자로 받는 메서드

---
+++ Chap. 3/3-31.js
@@ -0,0 +1,14 @@
+// 3-31
+// 콜백 함수와 함께 thisArg를 인자로 받는 메서드 
+
+Array.prototype.forEach(callback[, thisArg])
+Array.prototype.map(callback[, thisArg])
+Array.prototype.filter(callback[, thisArg])
+Array.prototype.some(callback[, thisArg])
+Array.prototype.every(callback[, thisArg])
+Array.prototype.findIndex(callback[, thisArg])
+Array.prototype.flatMap(callback[, thisArg])
+
+Array.prototype.from(arrayLike[,callback[, thisArg]])
+Set.prototype.forEach(callback[, thisArg])
+Map.prototype.forEach(callback[, thisArg])
