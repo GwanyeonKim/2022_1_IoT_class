4ff653caf88f7522a3f8866b6c4ba4f3f9d1c47e
Gwanyeon Kim <kky9711@cau.ac.kr>
2022-05-05 17:12:43 +0900

    log 수정

--- Chap.1/1-21.js
+++ Chap.1/1-21.js
@@ -11,23 +11,28 @@ var arr2 = [];
 arr2[1] = 1;
 
 arr1.forEach(function (v, i) {console.log(v,i);});  
-console.log('-')
 arr2.forEach(function (v, i) {console.log(v,i);}); 
 
 console.log('-------')
 
-arr1.map(function (v, i) {return v+i;}); 
-console.log('-')
-arr2.map(function (v, i) {return v+i;}); 
+b1 = arr1.map(function (v, i) {return v+i;}); 
+b2 = arr2.map(function (v, i) {return v+i;}); 
+
+console.log(b1);
+console.log(b2);
 
 console.log('-------')
 
-arr1.filter(function (v, i) {return !v;}); 
-console.log('-')
-arr2.filter(function (v, i) {return !v;}); 
+c1 = arr1.filter(function (v, i) {return !v;}); 
+c2 = arr2.filter(function (v, i) {return !v;}); 
+
+console.log(c1);
+console.log(c2);
 
 console.log('-------')
 
-arr1.reduce(function (p, c, i) {return p + c + i;}, ''); 
-console.log('-')
-arr2.reduce(function (p, c, i) {return p + c + i;}, ''); 
+d1 = arr1.reduce(function (p, c, i) {return p + c + i;}, ''); 
+d2 = arr2.reduce(function (p, c, i) {return p + c + i;}, ''); 
+
+console.log(d1);
+console.log(d2);
