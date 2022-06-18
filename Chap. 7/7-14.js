8f634491e8740185cf0864485d54fc7e537a912b
Gwanyeon Kim <kky9711@cau.ac.kr>
2022-06-19 01:33:08 +0900

    recommit

--- Chap. 7/7-14.js
+++ Chap. 7/7-14.js
@@ -2,6 +2,8 @@
 // 상위 클래스 접근 수단인 super 메서드 추가
 /*
 
+하위 클래스에서 상위 클래스의 프로토타입 메서드에 접근하는 수단
+다른 객체지향 언어들의 클래스 문법 중 하나인 'super'를 흉내낼꺼임
 
 
 
@@ -17,29 +19,39 @@ var extendClass = function(SuperClass, SubClass, subMethods) {
     if (!propName)
       return function() {
         SuperClass.apply(self, arguments);
-      };
+      }; 
+
     var prop = SuperClass.prototype[propName];
+
     if (typeof prop !== 'function') return prop;
     return function() {
       return prop.apply(self, arguments);
     };
+
   }; // 추가된 부분 끝
+
   if (subMethods) {
     for (var method in subMethods) {
       SubClass.prototype[method] = subMethods[method];
     }
+
   }
+
   Object.freeze(SubClass.prototype);
   return SubClass;
 };
+//------------------------
 
 var Rectangle = function(width, height) {
   this.width = width;
   this.height = height;
 };
+
 Rectangle.prototype.getArea = function() {
   return this.width * this.height;
 };
+//------------------------
+
 var Square = extendClass(
   Rectangle,
   function(width) {
@@ -51,6 +63,7 @@ var Square = extendClass(
     },
   }
 );
+
 var sq = new Square(10);
 sq.getArea(); // size is : 100
 console.log(sq.super('getArea')()); // 100
